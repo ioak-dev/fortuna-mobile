057af98f8302412dafd0a195a05caa2678fbbb57
Cs7dev <chinmayasm5@gmail.com>
2023-08-06 03:23:29 +0530

    auth service

--- lib/authentication/auth_service.dart
+++ lib/authentication/auth_service.dart
@@ -1,7 +1,38 @@
+// class AuthService {
+//   Future<bool> login(String username, String password) async {
+//     // call for authorization
+//     await Future.delayed(Duration(seconds: 10));
+//     return username == "Chinmay" && password == "Cr7";
+//   }
+// }
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class AuthService {
+  static const String apiUrl = 'https://api.ioak.io:8010/api';
+  static const String apiKey = 'fc2076f5-bee6-4b94-974f-5e110495b048';
+
   Future<bool> login(String username, String password) async {
-    // API call for authorization
-    await Future.delayed(Duration(seconds: 10));
-    return username == "Chinmay" && password == "Cr7";
+    final response = await http.post(
+      Uri.parse('$apiUrl/login'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: json.encode({
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Successfully authenticated
+      return true;
+    } else {
+      await Future.delayed(Duration(seconds: 10));
+     return username == "Chinmay" && password == "Cr7";
+
+    }
   }
 }
+
